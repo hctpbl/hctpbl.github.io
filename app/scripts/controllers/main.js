8b6f3abfc6106d4c1d7cf9a1191dac35cc3dd056
hpablos <hpablos@MacBook-Air-de-Hector.local>
2015-10-14 11:52:35 +0200

    Part two

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,27 @@
  */
 angular.module('styleSageCatsApp')
   .controller('MainCtrl', ['$scope', 'googleCatsService', function ($scope, googleCatsService) {
-    $scope.catClicks = 0;
+    $scope.catList = [
+      {
+        url: "http://weknowyourdreams.com/images/cat/cat-01.jpg",
+        name: "Cat 1",
+        clicks: 0
+      },
+      {
+        url: "http://weknowyourdreams.com/images/cat/cat-06.jpg",
+        name: "Cat 2",
+        clicks: 0
+      }
+    ];
+
 
     /*googleCatsService.getCatsFromGoogle()
       .then(function(result) {
         $scope.cat = result.data.responseData.results[0];
       });*/
 
-    $scope.clickCat = function() {
-      $scope.catClicks++;
+    $scope.clickCat = function(cat) {
+      cat.clicks++;
     };
 
   }]);
